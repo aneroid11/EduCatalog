4c26b41624d572347ca171cdaf96df992f939b29
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 16:40:52 +0300

    more info about genres and books

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-from .models import Book, BookInstance, Author
+from .models import Book, BookInstance, Author, Genre
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -9,12 +9,16 @@ def index(request: HttpRequest) -> HttpResponse:
     num_instances = BookInstance.objects.count()
     num_instances_available = BookInstance.objects.filter(status__exact="a").count()
     num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_books_with_jojo = Book.objects.filter(title__icontains="jojo").count()
 
     context = {
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
-        "num_authors": num_authors
+        "num_authors": num_authors,
+        "num_genres": num_genres,
+        "num_books_with_jojo": num_books_with_jojo,
     }
 
     return render(request, "catalog/index.html", context)
