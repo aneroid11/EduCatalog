24bd00c0021d80f0c9ea21bc35a46a5813fd1f89
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 08:24:02 +0300

    example model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class MyModel(models.Model):
+    my_field = models.CharField(max_length=20, help_text="Enter field")
+
+    class Meta:
+        ordering = ['-my_field']
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        return self.my_field
