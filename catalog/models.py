7d723f7e7152e0f828b6572e18060f4ddbe33986
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 10:06:42 +0300

    Book model

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-# from django.urls import reverse
+from django.urls import reverse
+from uuid import uuid4
 
 
 class Genre(models.Model):
@@ -7,3 +8,28 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Brief description")
+    isbn = models.CharField("ISBN", max_length=13, unique=True,
+                            help_text="13 Character "
+                                      "<a href='https://www.isbn-international.org/content/what-isbn'>"
+                                      "ISBN number"
+                                      "</a>")
+    genre = models.ManyToManyField(Genre, help_text="Select a genre to the book")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, help_text="Unique ID for this book in this library")
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
