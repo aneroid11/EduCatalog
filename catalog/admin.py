ad02734b8b1a8525c0ec5102aad76c5915157800
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 11:15:31 +0300

    change how the author appears in the admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import Book, Author, BookInstance, Genre, Language
 
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "date_of_birth", "date_of_death")
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Genre)
 admin.site.register(Language)
