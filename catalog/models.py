961a3ccffd3392b2799402ddb475e4a2ff3c0aa4
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 08:27:53 +0300

    the Genre model

--- catalog/models.py
+++ catalog/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-from django.urls import reverse
+# from django.urls import reverse
 
 
-class MyModel(models.Model):
-    my_field = models.CharField(max_length=20, help_text="Enter field")
-
-    class Meta:
-        ordering = ['-my_field']
-
-    def get_absolute_url(self):
-        return reverse('model-detail-view', args=[str(self.id)])
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="For example, Science Fiction or Horror")
 
     def __str__(self):
-        return self.my_field
+        return self.name
