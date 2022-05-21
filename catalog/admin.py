7608934b6ebdb14b917d5bd996681367dd6a4517
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 11:21:41 +0300

    change books look

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,7 +9,7 @@ class AuthorAdmin(admin.ModelAdmin):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("title", "author", "display_genre")
 
 
 @admin.register(BookInstance)
