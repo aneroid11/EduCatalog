b5aeb16895d8c6cdfb3bebd41c25517c120db103
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 10:18:16 +0300

    the Author model

--- catalog/models.py
+++ catalog/models.py
@@ -54,4 +54,18 @@ class BookInstance(models.Model):
         return f"{self.id} - {self.book.title}"
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
 
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField("Died", null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.last_name + ", " + self.first_name
