e353e6d38cf92b0fd7e06fd41aa858ef43a3e467
aneroid11 <a-e-v2003@yandex.by>
2022-05-21 12:00:33 +0300

    inline listing of Book items in the Author detail

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from .models import Book, Author, BookInstance, Genre, Language
 
 
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "date_of_birth", "date_of_death")
     fields = ["first_name", "last_name", ("date_of_birth", "date_of_death",)]
+    inlines = [BookInline]
 
 
 class BookInstanceInline(admin.TabularInline):
@@ -21,6 +27,7 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ("book", "status", "due_back", "id")
     list_filter = ("status", "due_back")
 
     fieldsets = (
