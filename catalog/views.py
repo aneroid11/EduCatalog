c75a900b509cf4e4cf04469924b5cec6efdc4e5d
aneroid11 <a-e-v2003@yandex.by>
2022-05-24 21:54:10 +0300

    basic views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "catalog/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # some more operations with context
+        return context
