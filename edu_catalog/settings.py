89aceb959b64d905ad9cffde33784990aeb5fdf9
aneroid11 <a-e-v2003@yandex.by>
2022-05-24 15:06:16 +0300

    start application

--- edu_catalog/settings.py
+++ edu_catalog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'catalog.apps.CatalogConfig',
+    'catalog.apps.CatalogConfig',
     'django_nose',
 ]
 
