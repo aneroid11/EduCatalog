d97db96fa1cfb593c6b0634c7e470a1897f43230
aneroid11 <a-e-v2003@yandex.by>
2022-05-25 13:15:06 +0300

    categories list

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("categories/", views.CategoriesView.as_view(), name="category-list"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 # from django.http import HttpResponse, HttpRequest
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Category
 
 
 class IndexView(TemplateView):
@@ -8,4 +9,12 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # some more operations with context
+        return context
+
+
+class CategoriesView(ListView):
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         return context
