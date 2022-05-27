92245fd868c663770828589b622b2adf634d8dba
aneroid11 <a-e-v2003@yandex.by>
2022-05-27 09:28:54 +0300

    password reset templates

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,14 +1,3 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
-
-class RenewBookForm(forms.Form):
-    user_search = forms.CharField(help_text="Введите ключевое слово или фразу")
-
-    def clean_user_search(self):
-        data = self.cleaned_data['user_search']
-
-        # check something here...
-        # if data < datetime.date.today():
-        #    raise ValidationError(_('Invalid date - renewal in past'))
-        return data
+# Create your forms here.
